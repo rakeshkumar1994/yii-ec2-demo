6a0d6ef5c9edc6616d646dd443d39fe19bba189b
rk0737800@gmail.com <rk0737800@gmail.com>
2017-05-25 11:54:56 +0530

    Yii code add student model5

--- controllers/StudentController.php
+++ controllers/StudentController.php
@@ -107,6 +107,7 @@ class StudentController extends Controller
     }
 
     /**
+	*fdgkdfgkl;sdf
      * Finds the Student model based on its primary key value.
      * If the model is not found, a 404 HTTP exception will be thrown.
      * @param integer $id
